c1ac4f3c6f4d8bcccb054130d71248aae347222c
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-02-13 10:30:13 -0800

    added port env variable

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ server.get('/', (req, res) => {
     res.status(200).json(songs);
 })
 
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 server.listen(port, () => console.log(`server is running on ${port}`))
